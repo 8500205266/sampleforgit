451ab8168bc07293e1decaad8ed7b54aa96baef7
Saidugari Prashanth <sprashanth@nisum.com>
2021-06-29 18:37:52 +0530

    intial commit

---
+++ src/main/java/com/example/sampleforgit/Commponenet.java
@@ -0,0 +1,12 @@
+package com.example.sampleforgit;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@RestController
+public class Commponenet
+{
+//ffffffff
+}
+
