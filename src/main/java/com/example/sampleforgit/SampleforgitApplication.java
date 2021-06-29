927ebf797744171c732223febd2fb01485cfd16d
Saidugari Prashanth <sprashanth@nisum.com>
2021-06-29 18:33:04 +0530

    Initial commit

---
+++ src/main/java/com/example/sampleforgit/SampleforgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleforgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleforgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleforgitApplication.class, args);
+	}
+
+}
